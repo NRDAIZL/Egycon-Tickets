3c3e495ac6b21332f2eda4372e3cef0a5633d0f3
xTrimy <Mohamed.ashraf881999@gmail.com>
2023-01-05 12:38:57 +0200

    Add onsite registration nav button

--- resources/views/components/sidebar-items.blade.php
+++ resources/views/components/sidebar-items.blade.php
@@ -113,6 +113,21 @@
               </a>
             </li>
             <li class="relative px-6 py-3">
+              @if($page == 'register')
+              <span
+                class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                aria-hidden="true"
+              ></span>
+              @endif
+              <a
+                class="inline-flex items-center w-full text-sm font-semibold text-gray-500 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-400"
+                href="{{ route('admin.register',$event_id) }}"
+              >
+                <i class="las la-plus text-xl"></i>
+                <span class="ml-4">Onsite Registration</span>
+              </a>
+            </li>
+            <li class="relative px-6 py-3">
               @if($page == 'reset-tickets')
               <span
                 class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
