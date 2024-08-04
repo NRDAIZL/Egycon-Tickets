16af92699bf692e88d4088a940437285b423eb99
xTrimy <Mohamed.ashraf881999@gmail.com>
2024-08-04 21:13:45 +0300

    chore: Refactor EnsureQueueListenerIsRunning command to start queue listener using nohup command

--- app/Console/Commands/EnsureQueueListenerIsRunning.php
+++ app/Console/Commands/EnsureQueueListenerIsRunning.php
@@ -92,7 +92,6 @@ class EnsureQueueListenerIsRunning extends Command
     {
         $output = [];
         $command =  'nohup php "' . base_path("artisan") . '" queue:work --daemon >> '. base_path('storage/logs/laravel-queue.log').' 2>&1 &';
-        $command =  'nohup php "' . base_path("artisan") . '" queue:work --daemon >>  2>&1 &';
         Log::info("command: " . $command);
         $pid = exec($command, $output);
         Log::info('t', $output);
